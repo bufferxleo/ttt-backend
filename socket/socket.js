b66ca9fe560a9bdbfae124986b963cae5e346a8f
leocodeio <basicleo@787>
2024-07-23 10:44:10 +0530

    added hashtag

--- socket/socket.js
+++ socket/socket.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["https://bufferxleo.github.io/ttt-frontend/"],
+    origin: ["https://bufferxleo.github.io/ttt-frontend/#/"],
     methods: ["GET", "POST"],
   },
 });
